68a4df17932d49ddecdf27152bd5b1eb209704d5
u1967700 <149961361+u1967700@users.noreply.github.com>
2024-03-08 15:14:31 +0100

    canvi jquery definitiu

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,16 @@
-$( "#play" ).on('click', function () {
+$( "#play" ).on( "click", function () {
     window.location.assign("./html/game.html");
 });
 
-$( "#options" ).on('click', function () {
+$( "#options" ).on( "click", function () {
     window.location.assign("./html/options.html");
 });
 
-$( "#saves" ).on('click', function () {
+$( "#saves" ).on( "click", function () {
     console.error("Opció no implementada");
 });
 
-$( "#exit" ).on('click', function () {
+$( "#exit" ).on( "click", function () {
     console.warn("No es pot sortir!");
 });
 
