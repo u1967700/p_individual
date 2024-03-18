b81ac5f4e55f48785e1658b7848a8c96cb028529
u1967700 <149961361+u1967700@users.noreply.github.com>
2024-03-18 16:38:16 +0100

    branch_nou

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,24 @@
-$( "#play" ).on( "click", function () {
-    window.location.assign("./html/game.html");
-});
-
-$( "#options" ).on( "click", function () {
-    window.location.assign("./html/options.html");
-});
-
-$( "#saves" ).on( "click", function () {
-    console.error("Opció no implementada");
-});
-
-$( "#exit" ).on( "click", function () {
-    console.warn("No es pot sortir!");
-});
-
-/*
 addEventListener('load', function() {
     document.getElementById('play').addEventListener('click', 
     function(){
+        sessionStorage.removeItem("save");
         window.location.assign("./html/game.html");
+    });
+
+    document.getElementById('saves').addEventListener('click', 
+    function(){
+        fetch("./php/load.php",{
+            method: "POST",
+            body: "",
+            headers: {"content-type":"application/json; charset=UTF-8"}
+        })
+        .then(response=>{
+            if (response.ok) response.text();
+            else trow("PHP connection fail");
+        })
+        .then(partida=>sessionStorage.save = partida)
+        .catch(err=>sessionStorage.save = localStorage.save)
+        .finally(()=>window.location.assign("./html/game.html"));
     });
 
     document.getElementById('options').addEventListener('click', 
@@ -26,14 +26,8 @@ addEventListener('load', function() {
         window.location.assign("./html/options.html");
     });
 
-    document.getElementById('saves').addEventListener('click', 
-    function(){
-        console.error("Opció no implementada");
-    });
-
     document.getElementById('exit').addEventListener('click', 
     function(){
         console.warn("No es pot sortir!");
     });
 });
-*/
